d32dc47c472e7681bbf7bc3f0facdc87d631fe51
Nabeel Ahmed <nabeel.amd93@gmail.com>
2024-05-13 12:53:00 +0300

    User Session Activity Count Added
    User Session Activity Count Added

--- src/app/_pages/dashboards/setting-dashboard/setting-dashboard.component.ts
+++ src/app/_pages/dashboards/setting-dashboard/setting-dashboard.component.ts
@@ -5,7 +5,8 @@ import {
     AlertService,
     SpinnerService
 } from 'src/app/_helpers';
-import { ApiCode,
+import {
+    ApiCode,
     AuthResponse,
     AuthenticationService,
     SettingService
@@ -27,6 +28,10 @@ export class SettingDashboardComponent implements OnInit {
     public REPORT_SETTING_STATISTICS: EChartsOption;
     public DASHBOARD_SETTING_STATISTICS: EChartsOption;
     public SERVICE_SETTING_STATISTICS: EChartsOption;
+    public SESSION_COUNT_STATISTICS: EChartsOption;
+    // daily session detail
+    public fromdate: any;
+    public todate: any;
 
     constructor(
         private alertService: AlertService,
@@ -34,16 +39,34 @@ export class SettingDashboardComponent implements OnInit {
         private settingService: SettingService,
         private authenticationService: AuthenticationService) {
         this.authenticationService.currentUser
-        .subscribe(currentUser => {
-            this.currentUser = currentUser;
-        });
+            .subscribe(currentUser => {
+                this.currentUser = currentUser;
+            });
     }
 
     ngOnInit(): void {
         this.fetchSettingDashboard({
-            username:  this.currentUser.username
+            username: this.currentUser.username
         });
     }
+
+    public generateRandomData(count: number): { key: string[], value: string[] } {
+        const key: string[] = [];
+        const value: string[] = [];
+        for (let i = 0; i < count; i++) {
+          const randomYear = Math.floor(Math.random() * (2025 - 2010)) + 2010;
+          const randomMonth = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
+          const randomDay = String(Math.floor(Math.random() * 28) + 1).padStart(2, '0');
+          const formattedDate = `${randomYear}-${randomMonth}-${randomDay}`;
+          key.push(formattedDate);
+          value.push((Math.random() * 1000).toFixed(2));
+        }
+        return {
+            key: key,
+            value: value
+        };
+    }
+      
 
     // fetch all lookup
     public fetchSettingDashboard(payload: any): any {
@@ -68,6 +91,9 @@ export class SettingDashboardComponent implements OnInit {
                     'Dashboard Setting', response.data['DASHBOARD_SETTING_STATISTICS']['data']);
                 this.SERVICE_SETTING_STATISTICS = this.fillChartByPayloadId(
                     'Form Setting', response.data['SERVICE_SETTING_STATISTICS']['data']);
+                this.SESSION_COUNT_STATISTICS = this.SESSION_COUNT_STATISTICS = 
+                    this.fillChartBySessionCount(response.data['SESSION_COUNT_STATISTICS']['data']);
+                    
             }, (response: any) => {
                 this.spinnerService.hide();
                 this.alertService.showError(response.error.message, ApiCode.ERROR);
@@ -77,24 +103,82 @@ export class SettingDashboardComponent implements OnInit {
     public fillChartByPayloadId(name: any, data: any): any {
         return {
             tooltip: {
-              trigger: 'item'
+                trigger: 'item'
             },
             legend: {
-              show: false 
+                show: false
             },
             series: [
-              {
-                name: name,
-                type: 'pie',
-                radius: ['40%', '60%'],
-                center: ['50%', '45%'],
-                data: data,
-                label: {
-                    formatter: '{b}: ({c})'
+                {
+                    name: name,
+                    type: 'pie',
+                    radius: ['40%', '60%'],
+                    center: ['50%', '50%'],
+                    data: data,
+                    label: {
+                        formatter: '{b}: ({c})'
+                    }
                 }
-              }
             ]
         };
+    }
+
+    public fillChartBySessionCount(data: any): EChartsOption {
+        return {
+            title: {
+                show: false
+            },
+            toolbox: {
+                show: false
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            grid: {
+                left: '7%',
+                right: '7%',
+                top: '12%',
+                bottom: '15%'
+            },
+            dataZoom: [
+                // {
+                //     type: 'inside',
+                //     // Change size of the inside zoom area
+                //     width: 20, // Set the width
+                //     height: 20 // Set the height
+                // },
+                // {
+                //     type: 'slider',
+                //     height: 10, // Set the height
+                //     bottom: 10, // Set the position from the bottom
+                // }
+            ],
+            xAxis: {
+                data: data.map((object: any) => object.key),
+                silent: false,
+                splitLine: {
+                    show: false
+                },
+                splitArea: {
+                    show: false
+                }
+            },
+            yAxis: {
+                splitArea: {
+                    show: false
+                }
+            },
+            series: [
+                {
+                    type: 'bar',
+                    data: data.map((object: any) => object.value),
+                    large: true,
+                }
+            ]
+        }
     }
 
 }
